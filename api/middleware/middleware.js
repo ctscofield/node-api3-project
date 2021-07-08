51fa82e846992e939d9632c94ac9f91916bd75f8
ctscofield <ctscofield@gmail.com>
2021-07-07 20:15:34 -0600

    set up and logger endpoint

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,30 @@
 function logger(req, res, next) {
-  // DO YOUR MAGIC
-}
+  const timeStamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timestamp}] ${method} to ${url}`);
+  next();
+};
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-}
+  console.log("validateUserId");
+  next();
+};
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
-}
+  console.log("validateUser");
+  next();
+};
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
-}
+  console.log("validatePost");
+  next();
+};
 
+module.exports = {
+  validateUserId,
+  validateUser,
+  validatePost,
+  logger
+};
 // do not forget to expose these functions to other modules
